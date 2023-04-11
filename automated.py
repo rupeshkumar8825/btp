f9dc900dd29cae118670354e0a0a64085f6351ed
rupeshkumar8825 <111901045@smail.iitpkd.ac.in>
2023-04-11 12:40:51 +0530

    Made the setup of formation of linear programming using first set of constraints

--- automated.py
+++ automated.py
@@ -2,6 +2,9 @@
 # function in order to solve the linear programming to give the correct answer for this purpose 
 
 from math import comb
+import numpy as np;
+import math
+
 # defining the input format 
 # the input will be for N(n, k, m : r)
 n = int (input("Enter the value of n total number of data points"));
@@ -43,9 +46,53 @@ secondRow = [1]*len(firstRow)
 A.append(firstRow)
 A.append(secondRow)
 
+# now we have to calculate c = (k-r, k-r-1, ...... 1)
+c = [];
 
+temp = k-r
+# using the for loop for this purpose 
+while(temp):
+    c.append(temp)
+    temp = temp-1;
+
+
+# now we have to calculate value of b 
+b = [];
+b.append(math.floor((k-1)/r)*comb(m, k-1))
+b.append(n)
 
 print("The matrix is as follows \n\n");
 print(A);
+
+print("The array c is \n", c);
+print("The array b is \n", b);
+
+# we have to find the transpose of A 
+Atranspose = np.array(A).T.tolist();
+# now we also have to multiply all the elements with -1 because linprog only supports the <= 
+Atranspose = (np.array(Atranspose) * -1).tolist();
+print("The transpose of A is as follows\n", Atranspose);
+
+# similarly we have to multiply all the elements of c with -1 
+c = (np.array(c)*-1).tolist();
+print("The updated c\n", c)
+
+
+# and now we can use the linprog function in order to solve the LP formed above using the first set of constraints for this purpose 
+# we will be filling these fields based on the dual LP that will be formed 
+objectiveFunction = b;
+lhsInEqualityCoefficient = Atranspose
+rhsInEqualityCoefficient = c 
+boundary = [(0, float('inf'))]*len(b)
+
+print("The final LP is as follows \n\n");
+print("The objectiveFunction = ", objectiveFunction)
+print("The lhsInEqualityCoefficient = ", lhsInEqualityCoefficient)
+print("The rhsInEqualityCoefficient = ", rhsInEqualityCoefficient)
+print("The boundary  = ", boundary);
+
+
+
+# optimizedValue = 
 
 # now we have to calculate the linear program based on the 
